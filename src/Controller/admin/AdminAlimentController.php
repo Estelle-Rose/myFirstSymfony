d03c647aa33c5129106244b12719da2b3c9e543e
Estelle-Rose <serenetzen@gmail.com>
2021-03-26 12:14:44 +0100

    delete aliment / CRUD de base ok

--- src/Controller/admin/AdminAlimentController.php
+++ src/Controller/admin/AdminAlimentController.php
@@ -26,7 +26,8 @@ class AdminAlimentController extends AbstractController
     }
 
 
-    // 2 routes pour une fonction
+
+    // 2 routes pour une double fonction
     #[Route('/admin/aliment/ajouter', name: 'add_aliment')]
     #[Route('/admin/aliment/modifier/{id}', name: 'update_aliment')]
     public function addOrUpdateAliment(Aliment $aliment = null,Request $request, EntityManagerInterface $manager ): Response
@@ -48,5 +49,20 @@ class AdminAlimentController extends AbstractController
             'form' => $form->createView(), // on souhaite ici uniquement envoyer la partie affichage du formulaire
             'modification'=> $aliment->getId() !== null // la clé modification renvoie true si il y a un aliment (s'il il y a un id) et false dans le cas contraire
         ]);
+    }
+    #[Route('/admin/aliment/{id}', name: 'delete_aliment')]
+    public function deleteAliment( EntityManagerInterface $manager, $id): Response
+    {
+        
+        $aliment = $manager->getRepository(Aliment::class)->find($id);
+        if (!$aliment) {
+            throw $this->createNotFoundException(
+                "L'aliment n'a pas été trouvé en base de données" .$id
+            );
+        }
+
+        $manager->remove($aliment);
+        $manager->flush();
+        return $this->redirectToRoute('admin_aliment');
     }
 }
