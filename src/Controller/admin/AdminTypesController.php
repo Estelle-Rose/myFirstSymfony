1715c52c96cd86352658741b1b7dc67093a56671
Estelle-Rose <serenetzen@gmail.com>
2021-04-01 14:20:18 +0200

    gestion erreur 404

--- src/Controller/admin/AdminTypesController.php
+++ src/Controller/admin/AdminTypesController.php
@@ -8,6 +8,7 @@ use App\Repository\TypeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
